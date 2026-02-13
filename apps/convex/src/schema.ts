17c14dc9803e4f6a85412472eebc7d651af58be4
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-13 16:30:06 +1100

    feat: schema

--- apps/convex/src/schema.ts
+++ apps/convex/src/schema.ts
@@ -2,7 +2,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 const schema = defineSchema({
-  games: defineTable({
+  quizzes: defineTable({
     difficulty: v.union(
       v.literal("easy"),
       v.literal("medium"),
