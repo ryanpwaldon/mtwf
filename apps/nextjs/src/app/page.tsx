cd1ba3d051b697a73fa37f2a2a40382d41ee2f1c
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-13 16:55:17 +1100

    feat: home

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@acme/ui/button";
+
 export default function HomePage() {
   return (
     <main className="px-4 pt-16">
@@ -14,6 +16,14 @@ export default function HomePage() {
         Test your knowledge<br></br>
         with up to ten freinds online.
       </h2>
+      <div className="mt-16 flex gap-4">
+        <Button className="" size="lg" variant="default">
+          Create a quiz
+        </Button>
+        <Button className="" size="lg" variant="outline">
+          Join a quiz
+        </Button>
+      </div>
     </main>
   );
 }
