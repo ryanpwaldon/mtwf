beec0acd8172f00f2dc0c892874add1bffb348c8
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-13 17:24:48 +1100

    feat: style

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -31,14 +31,14 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "bg-background text-foreground font-sans antialiased",
+          "text-foreground bg-white font-sans antialiased",
           inter.variable,
         )}
       >
         <ConvexProvider>
           <ThemeProvider>
             <div className="mx-auto flex min-h-screen max-w-xl flex-col p-4">
-              <div className="flex-1 bg-gray-100">{props.children}</div>
+              <div className="bg-background flex-1">{props.children}</div>
             </div>
             <Toaster />
           </ThemeProvider>
--- apps/nextjs/src/components/header.tsx
+++ apps/nextjs/src/components/header.tsx
@@ -1,20 +1,23 @@
 export function Header() {
   return (
-    <header className="p-4">
-      <div className="flex flex-row gap-1 select-none">
-        <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
-          <span className="text-primary-foreground text-sm font-bold">M</span>
+    <div>
+      <header className="p-4">
+        <div className="flex flex-row gap-1 select-none">
+          <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
+            <span className="text-primary-foreground text-sm font-bold">M</span>
+          </div>
+          <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
+            <span className="text-primary-foreground text-sm font-bold">T</span>
+          </div>
+          <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
+            <span className="text-primary-foreground text-sm font-bold">W</span>
+          </div>
+          <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
+            <span className="text-primary-foreground text-sm font-bold">F</span>
+          </div>
         </div>
-        <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
-          <span className="text-primary-foreground text-sm font-bold">T</span>
-        </div>
-        <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
-          <span className="text-primary-foreground text-sm font-bold">W</span>
-        </div>
-        <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
-          <span className="text-primary-foreground text-sm font-bold">F</span>
-        </div>
-      </div>
-    </header>
+      </header>
+      <div className="mx-4 border-b" />
+    </div>
   );
 }
