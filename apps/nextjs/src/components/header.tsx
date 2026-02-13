f2c798b65a9992d182e65fcc86828dae900d5f08
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-14 02:27:12 +1100

    feat: style

--- apps/nextjs/src/components/header.tsx
+++ apps/nextjs/src/components/header.tsx
@@ -3,16 +3,16 @@ export function Header() {
     <div>
       <header className="p-4">
         <div className="flex flex-row gap-1 select-none">
-          <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
+          <div className="bg-muted-foreground flex size-5 skew-y-12 items-center justify-center">
             <span className="text-primary-foreground text-sm font-bold">M</span>
           </div>
-          <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
+          <div className="bg-muted-foreground flex size-5 -skew-y-12 items-center justify-center">
             <span className="text-primary-foreground text-sm font-bold">T</span>
           </div>
-          <div className="bg-primary flex size-6 skew-y-12 items-center justify-center">
+          <div className="bg-muted-foreground flex size-5 skew-y-12 items-center justify-center">
             <span className="text-primary-foreground text-sm font-bold">W</span>
           </div>
-          <div className="bg-primary flex size-6 -skew-y-12 items-center justify-center">
+          <div className="bg-muted-foreground flex size-5 -skew-y-12 items-center justify-center">
             <span className="text-primary-foreground text-sm font-bold">F</span>
           </div>
         </div>
