da0d381334ac889afb599c8744094ae2b4746db5
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-14 17:16:29 +1100

    feat: updates

--- packages/ui/src/avatar.tsx
+++ packages/ui/src/avatar.tsx
@@ -75,7 +75,7 @@ function AvatarBadge({ className, ...props }: React.ComponentProps<"span">) {
         "bg-primary text-primary-foreground ring-background absolute right-0 bottom-0 z-10 inline-flex items-center justify-center rounded-full ring-2 select-none",
         "group-data-[size=sm]/avatar:size-2 group-data-[size=sm]/avatar:[&>svg]:hidden",
         "group-data-[size=default]/avatar:size-2.5 group-data-[size=default]/avatar:[&>svg]:size-2",
-        "group-data-[size=lg]/avatar:size-3 group-data-[size=lg]/avatar:[&>svg]:size-2",
+        "group-data-[size=lg]/avatar:size-4 group-data-[size=lg]/avatar:[&>svg]:size-2.5",
         className,
       )}
       {...props}
