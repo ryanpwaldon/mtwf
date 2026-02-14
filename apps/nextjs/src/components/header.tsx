d73dc2c052a251026eb6b2c341439dbfe8b83ab0
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-15 00:04:56 +1100

    feat: dark/light mode

--- apps/nextjs/src/components/header.tsx
+++ apps/nextjs/src/components/header.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
+import { ThemeToggle } from "@acme/ui/theme";
+
 import { Logo } from "./logo";
 
 export function Header() {
   return (
     <div>
-      <header className="flex justify-between border-b p-4">
+      <header className="flex items-center justify-between border-b p-4">
         <Link href="/">
           <Logo />
         </Link>
+        <ThemeToggle />
       </header>
     </div>
   );
