497a09068a5c049b746d406d8b94f4f55ecd2934
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-14 16:31:03 +1100

    feat: quiz page

--- apps/nextjs/src/app/quiz/page.tsx
+++ apps/nextjs/src/app/quiz/page.tsx
@@ -18,26 +18,39 @@ export default function QuizPage() {
 
   return (
     <div className="mx-auto flex min-h-screen max-w-xl flex-col p-4">
-      <div className="bg-background flex-1">
+      <div className="bg-background flex flex-1 flex-col">
         <Header />
-        <main className="p-4">
-          <Card className="w-full">
-            <CardHeader className="border-b">
-              <CardTitle>Avatar</CardTitle>
-              <CardDescription>Select your color</CardDescription>
-            </CardHeader>
-            <CardContent className="flex justify-center">
-              <Avatar size="lg" tooltip={limePersona.label}>
-                <AvatarFallback className={limePersona.color} />
-              </Avatar>
-            </CardContent>
-          </Card>
-          <div className="mt-4 flex gap-4">
-            <Button size="lg" variant="default" asChild>
-              <Link href="/question">Start</Link>
-            </Button>
+        <main className="flex-1 p-4">
+          <div className="flex gap-4">
+            <Card className="w-full">
+              <CardHeader className="border-b">
+                <CardTitle>Invite friends</CardTitle>
+                <CardDescription>Share the game code</CardDescription>
+              </CardHeader>
+              <CardContent className="flex justify-center">
+                <Avatar size="lg" tooltip={limePersona.label}>
+                  <AvatarFallback className={limePersona.color} />
+                </Avatar>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader className="border-b">
+                <CardTitle>Avatar</CardTitle>
+                <CardDescription>Select your color</CardDescription>
+              </CardHeader>
+              <CardContent className="flex justify-center">
+                <Avatar size="lg" tooltip={limePersona.label}>
+                  <AvatarFallback className={limePersona.color} />
+                </Avatar>
+              </CardContent>
+            </Card>
           </div>
         </main>
+        <div className="bg-background/95 sticky bottom-0 mt-4 flex gap-4 border-t p-4 backdrop-blur">
+          <Button size="lg" variant="default" asChild>
+            <Link href="/question">Start</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
