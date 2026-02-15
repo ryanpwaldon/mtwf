179b38c6d7838a8b481e87b3d3e5b6853fe8ce60
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-16 00:11:53 +1100

    feat: home style

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,33 +2,34 @@ import Link from "next/link";
 
 import { Button } from "@acme/ui/button";
 
-import { Header } from "~/components/header";
 import { PageShell } from "~/components/page-shell";
 
 export default function HomePage() {
   return (
     <PageShell>
-      <Header />
-      <main className="p-4 pt-16">
-        <h1 className="text-5xl font-extrabold tracking-tight">
-          Movie
-          <br />
-          Trivia
-          <br />
-          With
-          <br />
-          Friends
-        </h1>
-        <h2 className="text-muted-foreground mt-4">
-          Test your knowledge<br></br>
-          with up to ten freinds online.
-        </h2>
-        <div className="mt-16 flex gap-4">
-          <Button size="lg" variant="default" asChild>
-            <Link href="/quiz">Create a quiz</Link>
+      <main className="flex flex-1 flex-col items-center px-4 py-24">
+        <div className="2xs:w-1/2 aspect-2/3 w-3/4 bg-red-600 px-3 py-3">
+          <h1 className="2xs:text-4xl text-3xl leading-none font-extrabold tracking-tight">
+            Movie
+            <br />
+            Trivia
+            <br />
+            With
+            <br />
+            Friends
+          </h1>
+          <p className="mt-2 leading-snug text-red-100">
+            A game by
+            <br />
+            Ryan Waldon
+          </p>
+        </div>
+        <div className="2xs:w-1/2 flex w-3/4 flex-col gap-4 pt-12">
+          <Button size="xl" variant="default" asChild>
+            <Link href="/quiz">Create a game</Link>
           </Button>
-          <Button size="lg" variant="secondary" asChild>
-            <Link href="/join">Join a quiz</Link>
+          <Button size="xl" variant="outline" asChild>
+            <Link href="/join">Join a game</Link>
           </Button>
         </div>
       </main>
