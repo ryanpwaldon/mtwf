d2a4bc40075a9759f5d62b006e89601cf292b534
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-18 18:53:34 +1100

    feat: style

--- apps/nextjs/src/components/game-play.tsx
+++ apps/nextjs/src/components/game-play.tsx
@@ -165,7 +165,7 @@ function GamePlayInner({
     <PageShell>
       <header className="flex h-16 items-center justify-between border-b">
         <div className="flex h-full w-20 items-center justify-center">
-          <div className="bg-primary/10 text-muted-foreground flex size-7 items-center justify-center rounded-full text-center text-sm font-medium">
+          <div className="bg-primary/10 text-muted-foreground flex size-7 items-center justify-center rounded-md text-center text-sm font-medium">
             Q{game.currentQuestionIndex + 1}
           </div>
         </div>
@@ -189,7 +189,7 @@ function GamePlayInner({
           </motion.div>
         </div>
         <div className="flex h-full w-20 items-center justify-center">
-          <div className="bg-primary text-primary-foreground flex size-7 items-center justify-center rounded-full text-center text-sm font-medium">
+          <div className="bg-primary text-primary-foreground flex size-7 items-center justify-center rounded-md text-center text-sm font-medium">
             <NumberFlow value={secondsLeft} />
           </div>
         </div>
