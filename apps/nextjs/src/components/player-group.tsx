3b8f9643d23f10e71f84152ade7a79e76cf77b08
Ryan Waldon <ryanpwaldon@gmail.com>
2026-02-18 18:53:41 +1100

    feat: animation tweak

--- apps/nextjs/src/components/player-group.tsx
+++ apps/nextjs/src/components/player-group.tsx
@@ -49,6 +49,7 @@ export function PlayerGroup({
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
+                  transition={{ duration: 0.2, ease: "easeOut" }}
                 >
                   {badge}
                 </motion.div>
